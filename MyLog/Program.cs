3a8e94b23f0d6c12a0e874857a5776322d1211a6
mbaibatyr <murat_b@mail.ru>
2025-11-19 20:43:04 +0500

    ++

---
+++ MyLog/Program.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using Serilog.Core;
+
+namespace MyLog
+{
+    internal class Program
+    {
+        private static Logger logger;
+        static void Main(string[] args)
+        {
+            logger = new LoggerConfiguration()
+                .WriteTo.File("logs\\" + DateTime.Now.ToString("dd.MM.yyyy") + ".log",
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
